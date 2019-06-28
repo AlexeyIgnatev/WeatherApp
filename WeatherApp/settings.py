dbc668bbc7d5c446d6c70da11a34b20b1b139b5c
AlexeyIgnatev <79508350832L@gmail.com>
2019-06-28 12:31:48 +0400

    Initial commit

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '=t@a*_r=xcryc1qy!u&3m87fev+u%q+3v+f%rh51qyw)f$vo+4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
